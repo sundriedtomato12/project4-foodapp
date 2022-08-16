89d7ebd84afe0c134a4ca725d5d7567573307d5c
sundriedtomato12 <felicia.tanwp@gmail.com>
2022-08-16 19:46:24 +0800

    created basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function App() {
-  return <div>This is App.jsx</div>;
-}
+  return (
+    <>
+    <div class="header">
+      <h1>WhereToEat</h1>
+    </div>
+    <div class="towns">
+      <p>Town components</p>
+    </div>
+    <div class="reviews">
+      <p>Review components</p>
+    </div>
+    </>
+  )};
