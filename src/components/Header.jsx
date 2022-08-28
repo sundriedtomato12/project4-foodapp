0f9c56ac9fe0d45a8ff62b3934d302d88c363660
sundriedtomato12 <felicia.tanwp@gmail.com>
2022-08-29 01:48:09 +0800

    login, signup and logout functions are done. also included verification function to verify if the userId and loggedInHash match.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 
-export default function Header() {
-  return (
-    <>
-      <Grid container spacing={2} className="header">
-        <Grid item xs={8}>
-          <h1 className="header">Where To Eat?</h1>
-        </Grid>
+export default function Header({ user }) {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    axios
+      .post("/api/logout")
+      .then(() => {
+        navigate("/");
+        window.location.reload(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  let buttons = "";
+  if (user.loggedIn == false) {
+    buttons = (
+      <>
         <Grid item xs={2}>
           <Button variant="contained" className="login" href="/login">
             Login
@@ -19,6 +30,30 @@ export default function Header() {
             Sign Up
           </Button>
         </Grid>
+      </>
+    );
+  } else if (user.loggedIn == true) {
+    buttons = (
+      <Grid item xs={2}>
+        <Button
+          variant="contained"
+          color="error"
+          className="logout"
+          onClick={handleLogout}
+        >
+          Logout
+        </Button>
+      </Grid>
+    );
+  }
+
+  return (
+    <>
+      <Grid container spacing={2} className="header">
+        <Grid item xs={8}>
+          <h1 className="header">Where To Eat?</h1>
+        </Grid>
+        {buttons}
       </Grid>
     </>
   );
--- src/pages/TownPage.jsx
+++ src/pages/TownPage.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import Header from "../components/Header.jsx";
 import Row from "../components/Row.jsx";
 
-export default function TownPage() {
+export default function TownPage({ user }) {
   let { town_id } = useParams();
   const [stallsInTown, setStallsInTown] = useState([]);
   const [townName, setTownName] = useState("");
@@ -19,7 +19,7 @@ export default function TownPage() {
 
   return (
     <>
-      <Header />
+      <Header user={user} />
       <h1>{townName}</h1>
       <Row title="Stalls" stallsInTown={stallsInTown} />
     </>
